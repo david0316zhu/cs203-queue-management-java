377da2b036701bedad6d71b8b40a7089a351ece8
Jonathan-Teo <teo.junrui.jonathanedward2@gmail.com>
2023-09-16 17:36:11 +0800

    Added mock users

---
+++ db-utils/cs203-load-data-demo.sql
@@ -0,0 +1,17 @@
+use ticketmaster;
+## Add users
+insert into ticketmaster.user (user_id, mobile_number, email, password, authenticator_id, is_verified) values 
+(1, "06586616057", "david.zhu.2022@smu.edu.sg", "test123","001", 1),
+(2, "06597873327", "ryan.yap.2022@smu.edu.sg", "test123", "002", 1),
+(3, "06598231539", "jrteo.2022@smu.edu.sg", "test123", "002", 1),
+(4, "06581996653", "clarissatoh.2022@smu.edu.sg", "test123", "003", 1),
+(5, "06592366941", "benedictlee.2022@smu.edu.sg", "test123", "004", 1),
+(6, "00000000001", "teo.junrui.jonathanedward3@gmail.com", "test123", "005", 0),
+(7, "00000000002", "teo.junrui.jonathanedward4@gmail.com", "test123", "006", 0),
+(8, "00000000003", "teo.junrui.jonathanedward5@gmail.com", "test123", "007", 0),
+(9, "00000000004", "kaifengwoo3@gmail.com", "test123", "008", 0),
+(10,"00000000005", "jialingng676@gmail.com", "test123", "009", 1),
+(11,"00000000006", "fongzhengde03@gmail.com", "test123", "010", 1),
+(12,"00000000007", "sharifahbintehairulllizam@gmail.com", "test123", "011", 1),
+(13, "00000000008", "amandaxallen5@gmail.com", "test123", "012", 1);
+
