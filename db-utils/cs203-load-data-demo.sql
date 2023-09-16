8d2fb9f34be20e0ab81fe1aaccda0849939bf5e4
Jonathan-Teo <teo.junrui.jonathanedward2@gmail.com>
2023-09-16 18:52:11 +0800

    Added events data

--- db-utils/cs203-load-data-demo.sql
+++ db-utils/cs203-load-data-demo.sql
@@ -15,3 +15,61 @@ insert into ticketmaster.user (user_id, mobile_number, email, password, authenti
 (12,"00000000007", "sharifahbintehairulllizam@gmail.com", "test123", "011", 1),
 (13, "00000000008", "amandaxallen5@gmail.com", "test123", "012", 1);
 
+# select * from ticketmaster.event;
+# Add events
+insert into ticketmaster.event (event_id, event_name, max_queueable, is_highlighted, description, poster_image_path) values
+("tswift-era-2024", "Taylor Swift The Eras Tour", 3, 1, 
+"The Eras Tour is the ongoing sixth concert tour by American singer-songwriter Taylor Swift, 
+who described it as a journey through all of her musical \"eras\". A homage to her albums, 
+the Eras Tour is her most expansive tour yet, 
+with 146 dates across five continents. It is her second all-stadium tour after the 2018 Reputation Stadium Tour.", # 346 characters
+"taylor-swift.png"),
+("cplay-mots-2024", "Coldplay: Music of the Spheres Tour", 4, 1,
+"Music of the Spheres (subtitled Vol I. From Earth with Love) is the ninth studio album by British rock band Coldplay, 
+released on 15 October 2021 by Parlophone in the United Kingdom and Atlantic Records in the United States. It features guest appearances from Selena Gomez, 
+We Are King, Jacob Collier and BTS, as well as returning contributions from electronic producer Jon Hopkins.",
+"coldplay.jpg"),
+("gnr-2023", "Guns N' Roses: The We're F'N' Back! Tour", 2, 0, 
+"The We're F'N' Back! Tour is an ongoing concert tour by hard rock band Guns N' Roses.",
+"guns_and_roses.jpg" ),
+("anson-2023", "Anson Seabra: The Neverland Tour", 2, 0,
+"An emotive singer/songwriter, Anson Seabra makes artfully crafted and philosophical, piano-driven pop. 
+He emerged online in 2018 and garnered a strong social media fan base with songs like \"Broken\" and \"Emerald Eyes\". 
+A Kansas City, Missouri native, Seabra started taking piano lessons at age six and played in school bands growing up. 
+After high school, however, he pursued a computer science degree in college while continuing to write and record his own songs.",
+"the-neverland-tour.jpg"
+),
+("twice-rtb-2023", "TWICE: Ready to Be", 3, 1, 
+"Twice 5th World Tour \"Ready to Be\" is the third worldwide concert tour 
+and the fifth overall concert headlined by South Korean girl group Twice, in support of their twelfth extended play Ready to Be. 
+TWICE concert schedule 2023 includes multiple stops throughout the year, commencing with two nights at the KSPO Dome in Seoul on April 15 and 16. 
+TWICE will continue their tour in May with shows in Sydney, Melbourne, Osaka, and Tokyo.",
+"twice.jpg"),
+("greenwich-2023", "Greenwich Comedy Festival", 1, 0,
+"London's biggest comedy spectacular Greenwich Comedy Festival this year returns to the National Maritime Museum with stand-up's 
+finest live in the Big Top, plus craft beers and street eats and more. This year the festival celebrates its 15th anniversary.
+They're joined by the master of dry comedy Stewart Lee, the queen of ventriloquism Nina Conti, plus the likes of Fern Brady, 
+Russell Kane, Tim Key, plus Netflix star Phil Wang and many more.",
+"greenwich-2023.jpeg"),
+("andrea-b-2023", "Andrea Bocelli: Stifel", 5, 1,
+"Andrea Bocelli is one of the most recognizable voices in the world. For three decades, his lush arias and stylistic range 
+have made him the bestselling classical solo artist in history (selling 90 million albums worldwide). The blind, Tuscany-born tenor's 
+concerts are a testament to his range and crossover appeal,
+where his 2020 \"Music for Hope\" Easter concert was viewed 28 million times worldwide within the first 24 hours.",
+"andrea-bocelli.jpg"),
+("enhypen-2023", "Enhypen: Fate", 3, 0,
+"Enhypen is a South Korean boy band formed by Belift Lab, a former joint venture between CJ ENM and Hybe Corporation, through the 2020 survival competition show I-Land.
+The group is composed of seven members: Heeseung, Jay, Jake, Sunghoon, Sunoo, Jungwon, and Ni-ki. They debuted on November 30, 2020, with the extended play",
+"enhypen.jpeg"),
+("ed-sheeran-2024", "Ed Sheeran: The Mathematics Tour", 3, 0,
+"It's been a quick rise for soulful British singer/songwriter Ed Sheeran. He played his first club gig in New York City in early 2012, 
+and by the summer of 2013 tickets for his first headlining concert at New York's Madison Garden sold out in just three minutes. His song \"The A Team\" was nominated for a \"Song of the Year\" Grammy, and his debut album, \"+,\" 
+hit No. 5 on the US album chart. Sheeran's second album (2014) features famed producer Rick Rubin behind the board.",
+"ed-sheeran-2023.jpg"
+),
+("pink-sc-2024", "P!INK: Summer Carnival", 2, 0,
+"P!NK is one of pop music's most dynamic vocalists and stage performers. 
+Since releasing her debut studio album Can't Take Me Home in 2000, the three-time Grammy Award-winning artist (born Alecia Beth Moore) has sold 60 million albums worldwide.
+P!NK also cemented her status as a touring powerhouse when she wrapped 2018-19's Beautiful Trauma World Tour.",
+"pink-sc-2024.jpg"
+);
