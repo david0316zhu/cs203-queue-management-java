e60c96cd0f5f82d0f63202d97e9474d1671ca483
Jonathan-Teo <teo.junrui.jonathanedward2@gmail.com>
2023-09-23 01:32:04 +0800

    shortened API path

--- demo/src/main/java/com/ticketmasterdemo/demo/controller/UserController.java
+++ demo/src/main/java/com/ticketmasterdemo/demo/controller/UserController.java
@@ -5,9 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ticketmasterdemo.demo.common.exception.InvalidArgsException;
@@ -30,8 +29,8 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("/email/{email}/mobile/{mobile}")
-    public ResponseEntity<?> getUserByEmailAndMobile(@PathVariable String email, @PathVariable String mobile) {
+    @GetMapping("")
+    public ResponseEntity<?> getUserByEmailAndMobile(@RequestParam String email, @RequestParam String mobile) {
         try{
             User user = userService.getUser(email, mobile);
             return ResponseEntity.ok().body(user);
@@ -43,8 +42,8 @@ public class UserController {
         }
     }
 
-    @GetMapping("/email/{email}/mobile/{mobile}/is-verified")
-    public ResponseEntity<?> isUserVerified(@PathVariable String email, @PathVariable String mobile) {
+    @GetMapping("/is-verified")
+    public ResponseEntity<?> isUserVerified(@RequestParam String email, @RequestParam String mobile) {
         try {
             boolean success = userService.isUserVerified(email, mobile);
             return ResponseEntity.ok().body(success);
