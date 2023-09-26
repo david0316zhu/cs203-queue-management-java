2d94c0ab9a507ddb2d9242b68372a0ba53d08f96
David Zhu <david0316zhu@gmail.com>
2023-09-26 23:23:39 +0800

    bugs fix

--- demo/src/main/java/com/ticketmasterdemo/demo/configure/WebConfigure.java
+++ demo/src/main/java/com/ticketmasterdemo/demo/configure/WebConfigure.java
@@ -20,7 +20,7 @@ public class WebConfigure implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(jwtInterceptor)
                 .addPathPatterns("/users/**")
-                .addPathPatterns("/events-register/**")
+                //.addPathPatterns("/events-register/**")
                 .excludePathPatterns("/users/register")
                 .excludePathPatterns("/users/auth/login");
     }
